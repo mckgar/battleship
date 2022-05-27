2cf0ce2a227569dcf7d130ca2f21ec3ecbf8a5e5
mckgar <kmgarner23@outlook.com>
2022-05-27 00:06:01 -0600

    Updated player functions

--- src/player.js
+++ src/player.js
@@ -3,22 +3,54 @@ import gameboard from './gameboard';
 const player = (name, boardSize, isAi = false) => {
   const board = gameboard(boardSize, boardSize);
   const moveHistory = [];
-  for (let i = 0; i < boardSize; i += 1) {
-    const col = [];
-    for (let j = 0; j < boardSize; j += 1) {
-      col[j] = 0;
-    }
-    moveHistory[i] = col;
-  }
+  let lastHitX = -1;
+  let lastHitY = -1;
+  let triedAllAdj = false;
 
-  const getBoard = () => board;
+  const setMoveHistory = () => {
+    for (let i = 0; i < boardSize; i += 1) {
+      const col = [];
+      for (let j = 0; j < boardSize; j += 1) {
+        col[j] = 'E';
+      }
+      moveHistory[i] = col;
+    }
+  };
+  setMoveHistory();
+
+  const resetBoard = () => {
+    board.resetBoard();
+  };
+  const resetMoveHistory = () => {
+    const moveHistoryLength = moveHistory.length;
+    for (let i = 0; i < moveHistoryLength; i += 1) {
+      moveHistory.pop();
+    }
+    setMoveHistory();
+  };
+  const resetGame = () => {
+    resetBoard();
+    resetMoveHistory();
+  };
+
+  const getBoard = () => board.getBoard();
   const getMoveHistory = () => moveHistory;
   const getName = () => name;
-  const placePiece = (xCoord, yCoord, length) => {
-    board.placeShip(xCoord, yCoord, length);
+  const placePiece = (xCoord, yCoord, length, direction) => {
+    board.placeShip(xCoord, yCoord, length, direction);
   };
-  const isLegalMove = (xCoord, yCoord) => board.getBoard()[xCoord][yCoord] === 0 || board.getBoard()[xCoord][yCoord] === 'S';
-  const isLegalAttack = (xCoord, yCoord) => moveHistory[xCoord][yCoord] === 0;
+  const isLegalMove = (xCoord, yCoord) => {
+    if ((xCoord >= 0 && xCoord < boardSize) && (yCoord >= 0 && yCoord < boardSize)) {
+      return board.getBoard()[xCoord][yCoord] === 'E' || board.getBoard()[xCoord][yCoord] === 'S';
+    }
+    return false;
+  };
+  const isLegalAttack = (xCoord, yCoord) => {
+    if ((xCoord >= 0 && xCoord < boardSize) && (yCoord >= 0 && yCoord < boardSize)) {
+      return moveHistory[xCoord][yCoord] === 'E';
+    }
+    return false;
+  };
   const attack = (enemy, xCoord, yCoord) => {
     if (isLegalAttack(xCoord, yCoord)) {
       enemy.recieveAttack(xCoord, yCoord);
@@ -36,15 +68,90 @@ const player = (name, boardSize, isAi = false) => {
   };
   const isMiss = (xCoord, yCoord) => board.getBoard()[xCoord][yCoord] === 'M';
   const isHit = (xCoord, yCoord) => board.getBoard()[xCoord][yCoord] === 'H';
-  const makeMove = (enemy) => {
+  const hasLost = () => board.allSunk();
+  const makeMove = (enemy, prevX = -1, prevY = -1) => {
     if (isAi) {
       let makingMove = true;
       while (makingMove) {
+        if (prevX >= 0 && prevY >= 0) {
+          lastHitX = prevX;
+          lastHitY = prevY;
+          triedAllAdj = false;
+        }
+        if (lastHitX >= 0 && lastHitY >= 0 && !triedAllAdj) {
+          for (let i = 1; i < 6; i += 1) {
+            if (isLegalAttack(lastHitX + i, lastHitY)) {
+              attack(enemy, lastHitX + i, lastHitY);
+              makingMove = false;
+              return [lastHitX + i, lastHitY];
+            }
+            if (isLegalAttack(lastHitX - i, lastHitY)) {
+              attack(enemy, lastHitX - i, lastHitY);
+              makingMove = false;
+              return [lastHitX - i, lastHitY];
+            }
+            if (isLegalAttack(lastHitX, lastHitY + i)) {
+              attack(enemy, lastHitX, lastHitY + i);
+              makingMove = false;
+              return [lastHitX, lastHitY + i];
+            }
+            if (isLegalAttack(lastHitX, lastHitY - i)) {
+              attack(enemy, lastHitX, lastHitY - i);
+              makingMove = false;
+              return [lastHitX, lastHitY - i];
+            }
+          }
+          triedAllAdj = true;
+        }
+
         const xRand = Math.floor(Math.random() * boardSize);
         const yRand = Math.floor(Math.random() * boardSize);
         if (isLegalAttack(xRand, yRand)) {
           attack(enemy, xRand, yRand);
           makingMove = false;
+          return [xRand, yRand];
+        }
+      }
+    }
+    return null;
+  };
+  const randomShipPlacement = (length) => {
+    let placingShip = true;
+    while (placingShip) {
+      const xRand = Math.floor(Math.random() * boardSize);
+      const yRand = Math.floor(Math.random() * boardSize);
+      // Randomize direction; 0 horizontal, 1 vertical
+      const direction = Math.floor(Math.random() * 2);
+      let foundLegalSpace = false;
+      if (direction === 0) {
+        if (xRand + length <= boardSize) {
+          for (let i = xRand; i < xRand + length; i += 1) {
+            if (board.getBoard()[i][yRand] !== 'E') {
+              break;
+            }
+            if (i === xRand + length - 1) {
+              foundLegalSpace = true;
+            }
+          }
+        }
+        if (foundLegalSpace) {
+          placingShip = false;
+          placePiece(xRand, yRand, length, 'horizontal');
+        }
+      } else if (direction === 1) {
+        if (xRand + length <= boardSize) {
+          for (let i = yRand; i < yRand + length; i += 1) {
+            if (board.getBoard()[xRand][i] !== 'E') {
+              break;
+            }
+            if (i === yRand + length - 1) {
+              foundLegalSpace = true;
+            }
+          }
+        }
+        if (foundLegalSpace) {
+          placingShip = false;
+          placePiece(xRand, yRand, length, 'vertical');
         }
       }
     }
@@ -60,7 +167,10 @@ const player = (name, boardSize, isAi = false) => {
     recieveAttack,
     isMiss,
     isHit,
+    hasLost,
     makeMove,
+    randomShipPlacement,
+    resetGame,
   };
 };
 
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -31,6 +31,14 @@ test('Players can attack other players', () => {
   expect(player2.isMiss(2, 4)).toBeTruthy();
 });
 
+test('Players can attack other players ships', () => {
+  const player1 = player('Capt Cutter', 10);
+  const player2 = player('Capt Sisko', 10);
+  player2.placePiece(2, 4, 1, 'horizontal');
+  player1.attack(player2, 2, 4);
+  expect(player2.isHit(2, 4)).toBeTruthy();
+});
+
 test('AI players can attack other players', () => {
   const player1 = player('Capt Cutter', 3, 1);
   const player2 = player('Capt Sisko', 3);
@@ -44,4 +52,53 @@ test('AI players can attack other players', () => {
     }
   }
   expect(attacked).toBeTruthy();
+});
+
+test('Player reports correctly when all ships are sunk', () => {
+  const player1 = player('Capt Cutter', 3);
+  const player2 = player('Capt Sisko', 3);
+  player1.placePiece(1, 1, 1, 'horizontal');
+  player2.attack(player1, 1, 1);
+  expect(player1.hasLost()).toBeTruthy();
+});
+
+test('Player can randomly place ship', () => {
+  const player1 = player('Capt Keys', 3);
+  player1.randomShipPlacement(1);
+  let placed = false;
+  for (let i = 0; i < 3; i += 1) {
+    for (let j = 0; j < 3; j += 1) {
+      if (player1.getBoard()[i][j]) {
+        placed = true;
+      }
+    }
+  }
+  expect(placed).toBeTruthy();
+});
+
+test('Player can reset game', () => {
+  const player1 = player('Admiral Hood', 10);
+  player1.placePiece(2, 2, 2, 'vertical');
+  player1.placePiece(6, 7, 2, 'horizontal');
+  player1.recieveAttack(1, 3);
+  player1.recieveAttack(4, 9);
+  player1.resetGame();
+  let boardEmpty = true;
+  let moveHistoryEmpty = true;
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      if (player1.getBoard()[i][j] !== 'E') {
+        boardEmpty = false;
+      }
+    }
+  }
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      if (player1.getMoveHistory()[i][j] !== 'E') {
+        moveHistoryEmpty = false;
+      }
+    }
+  }
+  expect(boardEmpty).toBeTruthy();
+  expect(moveHistoryEmpty).toBeTruthy();
 });
